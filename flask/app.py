b80edf3b5579e1b98499fb33ec345d65e16dd841
raghav21r <raghav.rajpurohit1759@gmail.com>
2026-04-18 13:43:49 +0530

    Modified Web page

--- flask/app.py
+++ flask/app.py
@@ -11,5 +11,14 @@ def display():
 @flask.route('/dashboard')
 def dashboard():
     return render_template('dash.html')
+@flask.route('/welcome')
+def function_greet():
+    return f"""
+    <h2>
+        <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 text-center">
+            Verify,Will Your Knowledge Make You Placed
+        </div>
+    </h2>
+    """
 if __name__=="__main__":
     flask.run(debug=True)
